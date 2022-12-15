d9b2697f849c39c0ad8ea2888698b87ec57ea02e
Wilsantos1975 <Faustosantos@pursuit.org>
2022-12-14 20:59:16 -0500

    add average button functionality

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-
 const input = (button) => {
-    document.calc.display.value += button.value
-}
+  document.calc.display.value += button.value;
+};
 
 const clearDisplay = () => {
-    document.calc.display.value = calc.display.placeholder
-}
+  document.calc.display.value = calc.display.placeholder;
+};
 
 const enter = () => {
-    document.calc.display.value = eval(document.calc.display.value)
-}
+  document.calc.display.value = eval(document.calc.display.value);
+};
+
+const average = () => {
+  let sum = 0;
+  let listOfNums = document.calc.display.value.split(",");
+
+  for (const num of listOfNums) {
+    sum += parseInt(num);
+  }
+  let output = sum / listOfNums.length;
+
+  document.calc.display.value = output;
+};
